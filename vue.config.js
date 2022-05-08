b6286d4b0821c70d2d41cf87438d021f8457b9bf
Slava79198015981 <slava79198015981@gmail.com>
2022-05-08 23:14:54 +0400

    init

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: '/drag_and_drop_project/'
+  publicPath: '/drag_and_drop_project/',
+  transpileDependencies: true
 })
